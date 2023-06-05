7235bd6e36d35dec97ab0a9921eb69884c7b1e90
Ana Paula Urban <anapaulaurban.apu@gmail.com>
2023-06-05 16:26:10 -0300

    Create form en home: Autor, post y comentario

---
+++ project/apps/home/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from . import models
+
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model= models.Autor
+        fields= "__all__"
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model= models.Post
+        fields= "__all__"
+
+class ComentarioForm(forms.ModelForm):
+    class Meta:
+        model= models.Comentario
+        fields= "__all__"
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from . import forms
 
 def index(request):
     return render(request,'home/index.html')
+
+def crear_autor(request):
+    if request.method == "POST":
+        form=forms.AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+    else:
+        form= forms.AutorForm()
+        context= {"form": form}
+    return render(request, "home/crear_autor.html", context)
+
+def crear_post(request):
+    if request.method == "POST":
+        form=forms.PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+    else:
+        form= forms.PostForm()
+        context= {"form": form}
+    return render(request, "home/crear_post.html", context)
+
+def crear_comentario(request):
+    if request.method == "POST":
+        form=forms.ComentarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+    else:
+        form= forms.ComentarioForm()
+        context= {"form": form}
+    return render(request, "home/crear_comentario.html", context)
